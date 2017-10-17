8a429f5c0dd4d31fafeb25850c35ca4402dc2d13
xun <xunding@gmail.com>
2017-10-16 21:15:35 -0500

    add package.json in dist

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  All charts are modified in Vue.js from <a href="techanjs.org">TechJs.org</a>, a visual, stock charting (Candlestick, OHLC, indicators) and technical analysis library built on D3.
+  All charts are modified in Vue.js from <a href="http://techanjs.org">TechJs.org</a>, a visual, stock charting (Candlestick, OHLC, indicators) and technical analysis library built on D3.
 </div>
 </template>
 
